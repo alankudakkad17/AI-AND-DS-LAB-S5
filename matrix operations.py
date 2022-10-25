a53588ee0db465327d5be49bc661d9d92cbdd90e
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-10-25 09:13:33 +0530

    Create matrix operations.py

---
+++ matrix operations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import matplotlib.pyplot as plt p=np.array([[3,2],[8,5]]) q=np.array([(9,3],[6,4)))
+
+print("sum=",np.add(p,q))
+
+print("sub=" ‚np.subtract (p,q))
+
+print("multiply=",np.multiply(p,q))|
+
+print ("dot=" ,np.dot(p,q))
+
+print("division=",np.divide(p,q))
+
+print("transpose of matrix x=",np.transpose(x))
+
+print("transpose of matrix y=",np.transpose (y))
