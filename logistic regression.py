00fc0d2d6ab03f939899d455b3bfb85f9adaa75c
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-10-28 22:21:18 +0530

    Create logistic regression.py

---
+++ logistic regression.py
@@ -0,0 +1,13 @@
+import numpy
+from sklearn import linear_model
+
+#Reshaped for Logistic function.
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
+
+a=input("enter the tumor size")
+predicted = logr.predict(numpy.array([a]).reshape(-1,1))
+print('Cancerous') if predicted == 1 else print('Non-Cancerous')
